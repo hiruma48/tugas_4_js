3adb4d068a96faad454727311ae9e43ba9ee271e
tri ariyanto <triyanto129@gmail.com>
2023-05-06 10:24:20 +0700

    tugas 4 js
    mengumpulkan tugas

---
+++ tugas4.js/tugas4.js
@@ -0,0 +1,20 @@
+var desi = 160 ;
+var shani = 170 ;
+var ari = 180 ;
+
+if(desi>ari){
+    if(desi>shani){
+        console.log("desi lebih tinggi dari shani");
+    }
+    else{
+        console.log("desi adalah tinggi kedua");
+    }
+}
+else{
+    if(desi<shani){
+        console.log("desi lebih pendek dari shani");
+    }
+    else {
+        console.log("desi adalah lebih pendek ke dua");
+    }
+}
